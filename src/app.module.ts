d6f6a7f1fb53df26ae50e938bc4e153919cd9db7
Анатолий Мишин <lim0ncmon@yandex.ru>
2025-12-29 19:43:44 +0300

    auth mock

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { RepairRequestModule } from './repair-request/repair-request.module';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [RepairRequestModule],
+  imports: [RepairRequestModule, AuthModule, UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
